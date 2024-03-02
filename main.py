e04d44138e4a6c2d3662314372bf1b0ee46c0d69
Sajjad alDalwachee <sajjad.aldalwachee@gmail.com>
2024-03-02 03:49:54 +0300

    Add changes

--- main.py
+++ main.py
@@ -5,7 +5,14 @@ from pprint import pprint
 from datetime import datetime
 # from .facebook import facebook
 
-database = {
+
+COST = {
+    'raw':[],
+}
+
+SERVICE_TYPES = ['bandwidth', 'latency', None]
+
+DATABASE = {
     'youtube': {
         'request': {
             'ip': '',
@@ -36,9 +43,9 @@ database = {
 }
 
 def serverlog(name, clientip, path):
-    database[name]['request']['ip'] = clientip
-    database[name]['request']['requests'].append([clientip, path, datetime.now().strftime("%Y/%m/%d-%H:%M:%S")])
-    database[name]['request']['vistes'] += 1
+    DATABASE[name]['request']['ip'] = clientip
+    DATABASE[name]['request']['requests'].append([clientip, path, datetime.now().strftime("%Y/%m/%d-%H:%M:%S")])
+    DATABASE[name]['request']['vistes'] += 1
 
 # server
 def youtube(clientip, path):
@@ -60,20 +67,60 @@ def b2l30():
     return [2, 30]
 
 def b3l40():
-    # return 'Bandwith 3 mb and  Latency 40 ms'
+    # return 'bandwidth 3 mb and  Latency 40 ms'
     return [3, 40]
 
-# client
-def client(ip):
-    return f'Device{ip}'
+NETWORK_TREE = [
+    [b3l40],
+    [b3l40, b2l30],
+    [b2l30]
+]
+def sortpaths(path, via):
+     
+    if via == 'bandwidth':
+        path = sorted(path, key=lambda x: x[1])
+        return list(reversed(path))
+    elif via == 'latency':
+        return sorted(path, key=lambda x: x[2])
 
-def bp(size, max_latency):
+def map(tree=NETWORK_TREE):
+    bandwidth = 0
+    latency = 0
+    for path in tree:
+        for node in path:
+            bandwidth += node()[0]
+            latency += node()[1]
+
+        print(f'{[fn.__name__ for fn in path]} --- bandwidth : {bandwidth} , latency : {latency}')
+
+        COST['raw'].append([path, bandwidth, latency])
+
+        bandwidth = 0
+        latency = 0
+
+    COST['bandwith_sorted'] = sortpaths(COST['raw'], 'bandwidth')
+    COST['latency_sorted'] = sortpaths(COST['raw'], 'latency')
+
+def bp(service_type, size, max_latency):
     # do some calcu. to find the best path possable
     # [client, b3l40, b2l30, server]
-    return random.choice([[b3l40()], [b3l40(), b2l30()], [b2l30()]])
 
+    # return random.choice([[b3l40()],
+    #                       [b3l40(), b2l30()],
+    #                       [b2l30()]])
 
-def request(source='', dist=youtube, size=3, max_latency=100):
+    if service_type == 'bandwidth':
+        return COST['bandwith_sorted'][0]
+    elif service_type == 'latency':
+        return COST['latency_sorted'][0]
+    else:
+        return COST['bandwith_sorted'][0]
+
+def request(ip):
+    return f'Device{ip}'
+
+# client
+def client(source='', service_type=None, dist=youtube, size=3, max_latency=100):
 
     """
     distnation  : server
@@ -81,25 +128,26 @@ def request(source='', dist=youtube, size=3, max_latency=100):
     max_latency : max latency allowed for user experience
     """
 
-    client(source)
+    request(source)
 
-    path_list = bp(size, max_latency)
-
-    cost = {'bandwith':0, 'latency':0}
-
-    for p in path_list:
-        cost['bandwith'] += p[0]
-        cost['latency'] += p[1]
+    cost = bp(service_type, size, max_latency)
 
     # delay
-    sleep(cost['latency'] * 0.01)
+    sleep(cost[2] * 0.01)
 
-    print(f'{source} ... {cost} ... {dist(source, path_list)}')
+    print(f'{source} ... BW:{cost[1]}|L:{cost[2]} ... {dist(source, cost[0])}')
+
+
+# start mapping the network tree
+map(tree=NETWORK_TREE)
 
 for i in range(10):
-    request('192.168.0.1', random.choice([youtube, instagram, facebook]), 10, 35)
+    service = random.choice(SERVICE_TYPES)
+    print(f'Need good {service}')
+    client('192.168.0.1', random.choice(SERVICE_TYPES), random.choice([youtube, instagram, facebook]), 10, 35)
 
-pprint(database)
+# pprint(DATABASE)
+pprint(COST)
 
 
 
